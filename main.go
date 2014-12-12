a61219dd406d29cedb93c2ef6c5e3e4461546651
liz <liz@heroku.com>
2014-12-12 13:00:02 -0500

    Added Health endpoint

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func main() {
         fmt.Fprintf(w, "POLO!\n")
     })
 
+    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-type", "text/plain")
+        fmt.Fprintf(w, "yes good\n")
+    })
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
