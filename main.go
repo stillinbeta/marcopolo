3ed58f9086cdd75a41ea27ac5e31f228cecf3a44
liz <liz@heroku.com>
2015-09-10 10:35:52 -0400

    added log line

--- main.go
+++ main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-    "net/http"
-    "fmt"
-    "os"
-    "log"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    http.HandleFunc("/marco", func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Content-type", "text/plain")
-        w.Header().Set("X-Marco-Version", "0.0.3")
-        fmt.Fprintf(w, "POLO!\n")
-    })
+	log.Printf("%v\n", os.Args)
+	http.HandleFunc("/marco", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "text/plain")
+		w.Header().Set("X-Marco-Version", "0.0.3")
+		fmt.Fprintf(w, "POLO!\n")
+	})
 
-    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Content-type", "text/plain")
-        fmt.Fprintf(w, "yes good\n")
-    })
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8000"
-    }
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "text/plain")
+		fmt.Fprintf(w, "yes good\n")
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 
-    log.Fatal(http.ListenAndServe("0.0.0.0:" + port, nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, nil))
 }
